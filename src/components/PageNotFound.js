a177826aab4b6966946f77deb8943f063ba9b1b9
Alla Magina <babara@flylady.su>
2023-06-13 15:51:07 +0300

    feat: add new PageNotFound

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,16 +3,10 @@ import { Link } from 'react-router-dom';
 
 function PageNotFound() {
   return (
-    <div className='login'>
-      <div className='login__form'>
-        <h3 className='login__title'>
-          <span>404</span> - cтраница не найдена
-        </h3>
-        <p className='login__subtitle'>Ой, здесь ничего нет</p>
-        <Link className='login__button' to='/sign-in'>
-          Назад
-        </Link>
-      </div>
+    <div className='not-found'>
+      <Link className='not-found__button' to='/sign-in'>
+        Назад
+      </Link>
     </div>
   );
 }
